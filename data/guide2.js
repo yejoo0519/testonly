8ec2570bf5501045381d221f342a21a686649839
yejoo0519 <yejoo060519@gmail.com>
2026-04-24 16:50:59 +0900

    Add files via upload

---
+++ data/guide2.js
@@ -0,0 +1,165 @@
+const guideContent = {
+  id: 2,
+  title: "증명의 탑 공략",
+  created: "2025.02.23",
+  updated: "2025.02.23",
+  body: `
+    <div class="section-title">증명의 탑이란?</div>
+    <p>스토리 진행중 반드시 클리어 해야 하는 컨텐츠이며, 정수와 강림의 돌 수급을 위해 자주 사용할 컨텐츠입니다.</p>
+    <p>1층에서 51층까지 존재하며, 각 층별로 상대할 드래곤의 스펙의 편차가 있는 편입니다.</p>
+
+    <hr>
+
+    <div class="section-title">재료 파밍</div>
+    <p><img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/18.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">부활 회복제 60개</p>
+    <p><img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/19.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">크젬 60% 20개</p>
+    <div class="quote">크리티컬 젬의 경우 스토리에서는 사용하지 않습니다.</div>
+    <p>
+      <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/15.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">체력젬,
+      <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/16.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">공격젬,
+      <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/17.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">방어젬 각 30개씩
+    </p>
+    <p>인첸트 된 악몽, 황혼, 여명발톱 ( 가지고 있는 장신구가 파밍할 수 있는 장신구보다 낮을 경우 사용 )</p>
+    <p>등반시 사용할 강림, 진각성 타입에 따라 파밍합니다.</p>
+    <p><img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/20.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">어둠의 뿔 / 체력 또는 방어 인첸트 10개</p>
+    <p><img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/21.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">빛의 뿔 / 공격 인첸트 10개</p>
+    <div class="quote">스토리 증탑시에는 어둠, 빛뿔은 파밍하지 않아도 됩니다.</div>
+    <p>
+      <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/22.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">회피물약,
+      <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/23.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">크리물약 많이 ( 많을수록 좋습니다. )
+    </p>
+
+    <hr>
+
+    <div class="section-title">스토리 — 증탑 등반</div>
+    <p>스토리의 경우, 지급받은 진각성을 사용합니다. 솔라, 포폰 순서로 사용해줍니다.</p>
+    <p>젬의 경우 솔라 302 or 203, 포폰 032 or 023</p>
+    <div class="quote">드래곤 빌리지에서 젬을 알려줄땐 체 공 방 순서입니다.</div>
+    <p>장신구의 경우 부족한 스텟을 채워준다는 느낌으로 사용합니다.</p>
+    <p>1200 / 400 / 300 스텟을 기반으로 잡고, 직접 여러 장신구를 껴보시는걸 추천드립니다.</p>
+
+    <div class="section-title">1~20층</div>
+    <p>솔라와 포폰으로만 등반합니다.</p>
+    <p>솔라에게 7레벨 <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/23.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">크물 이나 <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/22.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">회물을 사용해줍니다.</p>
+    <p>대략 부활 회복제를 10개 언저리 사용하게 됩니다.</p>
+
+    <div class="section-title">21~40층</div>
+    <p>솔라, 제피로스, 포폰 순으로 사용하며</p>
+    <div class="quote">
+      솔라 302 or 203<br>
+      제피로스 113 or 104<br>
+      포폰 032 or 023
+    </div>
+    <p>장신구는 마찬가지로 부족한 스텟을 채우는데에 사용합니다.</p>
+
+    <div class="section-title">41~49층</div>
+    <p>41~49층은 진각성, 트스(SSS) 드래곤이 나옵니다.</p>
+    <p>물약이 충분하다면 41층 이전부터 사용해도 되지만, 41층 이상부터 8단계 물약을 사용합니다.</p>
+    <p>21~40층과 동일한 셋팅을 사용하며 물약은 8레벨을 사용합니다.</p>
+    <p>진각성 층의 경우 상성을 이용하여 비교적 쉽게 클리어 할 수 있습니다.</p>
+    <div class="quote">
+      체력 > 체방 > 체공 > 방어 > 공방 > 공격 > 체력<br>
+      체력은 체방형과 싸울때 강해지고, 체방형은 체공형과 싸울때 강해집니다.
+    </div>
+    <p>스토리 증탑의 경우 50층 도달시 스토리가 진행되며 별도의 셋팅 없이도 클리어가 됩니다.</p>
+
+    <hr>
+
+    <div class="section-title">스토리X 일반 증탑</div>
+    <p><span class="highlight">1층~10층</span> — 강림(진각) 한마리로만 등반합니다. <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/23.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">크물과 <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/22.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">회물이 부족한 경우 젬을 파밍하며 나온 스텟 물약을 사용합니다.</p>
+    <p><span class="highlight">11층~20층</span> — 드슬에서 쓰지않는 재물 드래곤을 첫번째 자리에 사용합니다. 부복제로 살리지 않아도 되는 말 그대로 재물이 될 드래곤.</p>
+    <p><span class="highlight">21층~30층</span> — 강림(진각) 3마리를 세팅해서 올라가되, 물약이 아깝기 때문에 맨 앞 첫번째 용만 물약을 사용합니다. 올라가며 죽는것은 당연합니다.</p>
+    <p><span class="highlight">31층~44층</span> — 전 층과 동일하게 3마리를 셋팅한 상태로 진행합니다. 물약은 첫번째와 두번째 드래곤에게만 사용합니다.</p>
+    <p><span class="highlight">45층~49층</span> — 전과 동일하게 3마리로 세팅하되 물약을 전부 먹입니다. ( 진각성 사용시 상성을 이용하면 좋습니다. )</p>
+
+    <hr>
+
+    <div class="section-title">헬카, 드슬 셋팅 — 앞라인 (탱커)</div>
+    <p>체력, 방어, 체방형을 추천합니다.</p>
+    <p>방어 350을 넘길 정도로만 방어에 투자 후 체력으로 몰빵합니다.</p>
+    <p>장신구는 <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/20.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">어둠의 뿔 체력 또는 방어 인첸트를 사용합니다.</p>
+    <p>강림이나 스텟이 좋은 드래곤의 경우 중앙 배치</p>
+    <p>물약의 경우 <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/22.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">회피 물약 고정으로 사용합니다.</p>
+
+    <div class="section-title">헬카, 드슬 셋팅 — 뒷라인 (딜러)</div>
+    <p>공격, 체공, 공방형을 추천합니다.</p>
+    <p><img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/16.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">공젬 3개, <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/19.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">크젬 2개 사용합니다. 공격 각성의 경우 공젬2개, 크젬3개를 사용해도 됩니다.</p>
+    <p>장신구는 <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/21.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">빛의 뿔 공격 인첸트 사용합니다.</p>
+    <p>공격력이 특별히 높은 드래곤의 경우 구석에 배치</p>
+    <p>물약의 경우 <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/23.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">크물을 고정으로 사용합니다.</p>
+
+    <div class="section-title">엔더 셋팅</div>
+    <p>헬카, 드슬 셋팅과 동일하나, 바람속성 탱커를 앞라인으로 세워야 클리어 확률이 눈에띄게 증가합니다.</p>
+    <p>탱커의 방어력도 370~400대로 맞춰주시면 좋습니다.</p>
+
+    <div class="section-title">딜러가 없어요 / 탱커가 부족해요</div>
+    <p>어둠 교배용 — 딜러로 채용 가능 ( 공격력 274 )</p>
+    <p>빛 교배용 — 탱커로 채용 가능 ( 방어력이 높습니다 )</p>
+
+    <div class="quote">해당 공략은 다른 유저분들의 글을 참고하며, 글쓴이의 주관이 꽤 많이 담긴편입니다. 수정해야할 부분이 있다면 지적해주세요.</div>
+
+    <hr>
+
+    <div class="section-title">하드 증탑 — 준비물</div>
+    <p><img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/18.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">부활회복제 60개</p>
+    <p>
+      <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/16.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">공격젬 10~20개 /
+      <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/19.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">크리젬 30~40개
+    </p>
+    <div class="quote">
+      딜러진 공젬 2개 크젬 3개 사용시 → 공젬 20개, 크젬 30개 파밍<br>
+      딜러진 공젬 1개 크젬 4개 사용시 → 공젬 10개, 크젬 40개 파밍
+    </div>
+    <p><img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/17.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">방어젬 10~20개</p>
+    <p><img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/15.png" style="width:18px;height:18px;vertical-align:middle;margin-right:4px">체력젬 40~50개</p>
+    <p><img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/20.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">어둠의 뿔 (회피/체력 or 방어)에 방첸 + 체첸 총 합 10개 ( 50, 51층 탱커 장신구 )</p>
+    <p><img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/21.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">빛의 뿔 공첸 10개 — 옵션 상관없이 인첸트는 공격 인첸트 고정 ( 50, 51층 딜러 장신구 )</p>
+    <p>
+      <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/23.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">크물 7단계,
+      <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/22.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">회물 7단계 넉넉하게 100개 이상
+    </p>
+    <p>크물 8단계, 회물 8단계 넉넉히 20개 이상 ( 50, 51층에서 사용 )</p>
+
+    <hr>
+
+    <div class="section-title">하드 증탑 — 등반</div>
+
+    <p><span class="highlight">1층~20층</span></p>
+    <div class="quote">
+      1~10층 : SS<br>
+      11~20층 : 풀각성
+    </div>
+    <p>풀강림 중 두마리를 1번과 2번에 배치, 셋팅 후 첫번째 드래곤에게만 물약을 먹여 등반하시면 됩니다.</p>
+
+    <p><span class="highlight">21~25층 : 풀초월</span></p>
+    <p>이제부터 고기방패(재물)를 사용합니다. 첫번째 드래곤 대신 본인이 가져온 재물용 하나를 넣습니다.</p>
+    <div class="quote">예) 재물 / 메인 / 메인</div>
+    <p>레벨이 낮은 순서대로 사용해주시면 됩니다.</p>
+
+    <p><span class="highlight">31~35층 : 진각성</span></p>
+    <p>진각성층 부터는 3번째 드래곤에게도 물약을 사용합니다.</p>
+    <p>재물이 떨어진 경우, 세마리 전부 강림 셋팅 후 등반하시면 됩니다.</p>
+
+    <p><span class="highlight">36~49층 : 강림층</span></p>
+    <p>41층부터는 매우 강력해집니다. 36~40층은 7물약 사용, 41층 부터는 8물약 사용해줍니다.</p>
+    <div class="quote">41층 전에 부복제가 20개 미만일 경우 운에 맡겨야합니다.</div>
+    <p>최소 탱커 6마리 이상 남도록 계산하며 등반합니다.</p>
+
+    <div class="section-title">하드 증탑 — 50, 51층</div>
+    <p><span class="highlight">앞라인 셋팅</span> — 방어 400이상, 나머지는 체력에 몰빵해주세요.</p>
+    <div class="quote">
+      어둠의 뿔<br>
+      체방일 경우 → (회피/체력 방첸)<br>
+      방어일 경우 → (회피/체력 체첸)<br>
+      체력일 경우 → (회피/방어 방첸)
+    </div>
+    <p>물약은 <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/22.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">회피물약 사용해주시면 됩니다.</p>
+
+    <p><span class="highlight">뒷라인 셋팅</span> — 취향에 따라 공젬 3 크젬 2 또는 공젬 1 크젬 4 사용해주시면 됩니다.</p>
+    <div class="quote">
+      빛뿔 — 공격옵션 공인첸 사용<br>
+      ( 옵션은 상관 없긴합니다. 공격 추천드려요 )
+    </div>
+    <p>물약은 <img src="https://raw.githubusercontent.com/yejoo0519/dogam/refs/heads/main/icon/23.png" style="width:18px;height:18px;vertical-align:middle;margin-right:2px">크리물약 사용해주시면 됩니다.</p>
+  `
+};
